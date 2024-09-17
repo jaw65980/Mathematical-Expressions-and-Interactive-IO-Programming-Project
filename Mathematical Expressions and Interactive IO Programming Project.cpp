ba20e38e7934d2ac5b97a8a6de621484b8cd848f
Jaden White <jaw65980@email.vccs.edu>
2024-09-17 18:55:25 -0400

    Add project files.

---
+++ Mathematical Expressions and Interactive IO Programming Project.cpp
@@ -0,0 +1,11 @@
+// Mathematical Expressions and Interactive IO Programming Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
