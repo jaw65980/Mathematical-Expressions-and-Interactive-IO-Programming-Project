bb95bfbd4a2365c9d8853896808707ee162e7201
Jaden White <jaw65980@email.vccs.edu>
2024-09-17 19:40:31 -0400

    Program started

--- Mathematical Expressions and Interactive IO Programming Project.cpp
+++ Mathematical Expressions and Interactive IO Programming Project.cpp
@@ -6,6 +6,13 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int CookieCount = 0;
+	cout << "Enter the number of cookies you'd like to bake. ";
+	cin >> CookieCount;
+	float CookieCountSol = CookieCount;
+	float Sugar = CookieCountSol *(1.5 / 48);
+	float Butter = 1 * (CookieCountSol / 48);
+	float Flour = CookieCountSol * (2.75 / 48);
+	cout << "\nTo bake " << CookieCount << " cookies, you will need: \n" << Sugar << " cups of sugar \n" << Butter << " cups of butter \n" << Flour << " cups of flour \n";
 }
 
