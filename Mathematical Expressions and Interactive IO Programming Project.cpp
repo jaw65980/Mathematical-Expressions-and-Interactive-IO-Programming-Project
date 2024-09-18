dadf6af5d1c1183cfba0e98e5739ffffa5d5d27e
Jaden White <jaw65980@email.vccs.edu>
2024-09-17 21:33:41 -0400

    Program started, going back to edit first program

--- Mathematical Expressions and Interactive IO Programming Project.cpp
+++ Mathematical Expressions and Interactive IO Programming Project.cpp
@@ -6,6 +6,19 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    float Principal;
+    float Rate;
+    float T;
+    float SavingsAmount;
+    cout << "What is the principal amount of money in your savings account?";
+    cin >> Principal;
+    cout << "What is the interest rate your account has?";
+    cin >> Rate;
+    cout << "How many times does your interest compound yearly?";
+    cin >> T;
+    SavingsAmount = 1 + (Rate / T);
+    SavingsAmount = pow(SavingsAmount, T);
+    SavingsAmount = Principal * SavingsAmount;
+    cout << SavingsAmount << "\n";
 }
 
